7401552d7053fe95d67e3a95944a567b9816c07b
Petar Mirchev <petar.g.mirchev@gmail.com>
2024-03-10 14:57:00 +0200

    add angular 17 & fontawesome to project
    convert Angular to v17 & add Fontawesome icons in project for navbar

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCoffee, faMagnifyingGlass, faCartShopping, faXmark, faUser, faShop } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, FontAwesomeModule], 
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'client';
+  faCoffee = faCoffee;
+  faMagnifyingGlass = faMagnifyingGlass;
+  faCartShopping = faCartShopping;
+  faXmark = faXmark;
+  faUser = faUser;
+  faShop = faShop;
 }
