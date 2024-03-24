385e1363b9338fb1ac8468e1cd733819b5af53aa
Petar Mirchev <petar.g.mirchev@gmail.com>
2024-03-24 20:30:14 +0200

    show info in profile page
    show info in profile page for user

--- client/src/app/pages/profile/profile.component.ts
+++ client/src/app/pages/profile/profile.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../auth/auth.service';
+
 
 @Component({
   selector: 'app-profile',
@@ -9,7 +10,11 @@ import { AuthService } from '../../auth/auth.service';
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+
+
+  token: string | null = null;
+  userProfileData: any = {}; // Placeholder for profile data
 
   constructor(private auth: AuthService, private router: Router) {}
 
@@ -17,8 +22,23 @@ export class ProfileComponent {
   ngOnInit(): void { //! guard if user is not login! (no access)
     if (!this.auth.isLoggedIn()) {
       this.router.navigate(['home']);
+    } else {
+        this.token = localStorage.getItem('token');
+        // (Optional) Check if token exists before proceeding
+        if (this.token) {
+          this.userProfileData =  JSON.parse(this.token);
+          //console.log(this.userProfileData);
+          
+        } else {
+          console.log('No token found in localStorage.');
+          // Handle the case where no token is present (e.g., redirect to login)
+          this.router.navigate(['login']);
+        }
     }
+
   }
 
-  
 }
+
+
+
